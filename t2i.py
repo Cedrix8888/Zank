4bf7729026b82c2289c9e1e74ca29139d33641a0
Donaldzhx <zx88485575@outlook.com>
2025-08-03 16:44:34 +0800

    feat(t2i.py): add a branch for cuda

--- t2i.py
+++ t2i.py
@@ -12,7 +12,7 @@ from lib_layerdiffuse.vae import TransparentVAEDecoder, TransparentVAEEncoder
 # Load models
 # RealVisXL_V4.0 is a specific version of SDXL
 # We use float16 and fp16 for more compatibility and less memory usage
-device = "mps"
+device = "cuda"
 sdxl_name = 'SG161222/RealVisXL_V4.0'
 tokenizer = CLIPTokenizer.from_pretrained(
     sdxl_name, subfolder="tokenizer")
@@ -62,14 +62,14 @@ pipeline = KDiffusionStableDiffusionXLPipeline(
 
 with torch.inference_mode():
     guidance_scale = 7.0
-    rng = torch.Generator().manual_seed(12345)
-    # rng = torch.Generator(device=device).manual_seed(12345)
-    # text_encoder.to(device)
-    # text_encoder_2.to(device)
-    # unet.to(device)
-    # vae.to(device)
-    # transparent_decoder.to(device)
-    # transparent_encoder.to(device)
+    # rng = torch.Generator().manual_seed(12345)
+    rng = torch.Generator(device=device).manual_seed(12345)
+    text_encoder.to(device)
+    text_encoder_2.to(device)
+    unet.to(device)
+    vae.to(device)
+    transparent_decoder.to(device)
+    transparent_encoder.to(device)
     
     positive_cond, positive_pooler = pipeline.encode_cropped_prompt_77tokens(positive_prompt)
     negative_cond, negative_pooler = pipeline.encode_cropped_prompt_77tokens(default_negative)
